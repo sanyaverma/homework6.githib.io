499d5c5c219a1cc52761f1a12be2dd4855f31fb2
Sanya Verma <verma.sanya@gmail.com>
2020-11-19 22:16:51 -0500

    update

--- js/video.js
+++ js/video.js
@@ -1,14 +1,46 @@
 var video;
+var volume;
 
-window.addEventListener("load", function() {
-	video = document.querySelector("#myVideo");
+window.addEventListener("load", function () {
+  video = document.querySelector("#myVideo");
+  console.log("Starting");
+  console.log(video.volume);
 });
 
-document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+//play
+document.querySelector("#play").addEventListener("click", function () {
+  console.log("Play Video");
+  video.play();
+  let vol = (doccument.querySelector("#volume").innerHTML = "100%");
 });
 
-document.querySelector("#pause").addEventListener("click", function() {
-	
+//pause
+document.querySelector("#pause").addEventListener("click", function () {
+  console.log("Pause Video");
+  video.pause();
 });
 
+// Volume Slider
+document.querySelector("#volumeSlider").addEventListener("change", function () {
+  console.log(this);
+  video.volume = this.value / 100;
+  volume.innerHTML = this.value + "%";
+  console.log(this.value);
+});
+
+//Slow down
+doccument.querySelector("#slower").addEventListener("click", function () {
+  console.log("Slow Down");
+  video.playbackRate *= 0.9;
+  console.log(video.playbackRate);
+});
+
+//Speed up
+doccument.querySelector("#faster").addEventListener("click", function () {
+  console.log("Speed Up");
+  video.playbackRate /= 0.9;
+  console.log(video.playbackRate);
+});
+
+//Skip ahead
+//doccument.querySelector("#skip").addEventListener("click", function () {});
